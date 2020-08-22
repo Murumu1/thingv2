dd9f1bb62696e5b0c54c6291ccff5efac940f4a8
Murtaza Daudali <murumu1@yahoo.com>
2020-08-22 23:49:38 +0100

    updated all db commands with dataset

--- thingv2/index.py
+++ thingv2/index.py
@@ -1,8 +1,9 @@
+from dataset import Table
+from discord import Message, Embed
 from discord.ext import commands
-import discord
 import random
-import sqlite3
 import typing
+import dataset
 
 import json
 
@@ -11,27 +12,27 @@ with open('config.json') as f:
 
 bot = commands.Bot(command_prefix='!')
 
-db = sqlite3.connect('economy')
-cursor = db.cursor()
+db = dataset.connect('sqlite:///economy')
+users: Table = db['users']
+games: Table = db['ttt_games']
 
 
-async def send_embed(ctx, message,
+async def send_embed(ctx, message: str,
                      colour: typing.Optional[str] = None,
                      title: typing.Optional[str] = None,
                      footer: typing.Optional[str] = None,
                      image: typing.Optional[str] = None):
-
     r, g, b = random.randint(120, 220), random.randint(120, 220), random.randint(120, 220)
     rd_col = "{:02x}{:02x}{:02x}".format(r, g, b)
-    embed = discord.Embed(description=message,
-                          color=(int(rd_col, 16) if not colour else int(colour, 16)),
-                          title=title)
+    embed = Embed(description=message,
+                  color=(int(rd_col, 16) if not colour else int(colour, 16)),
+                  title=title)
     if footer:
         embed.set_footer(text=footer)
     if image:
         embed.set_image(url=image)
 
-    await ctx.send(embed=embed)
+    return await ctx.send(embed=embed)
 
 
 @bot.event
@@ -49,7 +50,7 @@ async def on_message(message):
 
 @bot.event
 async def on_command_error(ctx, error):
-    print(error)
+    raise error
     error = getattr(error, 'original', error)
 
     if isinstance(error, commands.UserInputError):
@@ -88,34 +89,27 @@ class Economy(commands.Cog):
                       aliases=["balance", "account"],
                       help="Used to check your balance. Shows the amount of money you currently own.")
     async def bal(self, ctx):
-        cursor.execute('SELECT * FROM users WHERE id = {}'.format(ctx.author.id))
-        row = cursor.fetchone()
+        row = users.find_one(id=ctx.author.id)
 
         if not row:
-            cursor.execute('''INSERT INTO users(id, nick, balance) 
-            VALUES ({}, '{}', {})'''.format(ctx.author.id, ctx.author.name, 0))
-            db.commit()
-
+            users.insert(dict(id=ctx.author.id, nick=ctx.author.name, balance=0))
             await send_embed(ctx, 'I have set up your account!\nBalance: ${}'.format(0))
             return
         else:
-            await send_embed(ctx, 'Balance: ${}'.format(row[2]))
+            await send_embed(ctx, 'Balance: ${}'.format(row['balance']))
 
     @commands.command(aliases=["add", "add_money"],
                       hidden=True)
     @commands.has_permissions(administrator=True)
     async def gain(self, ctx, amount):
-        cursor.execute('SELECT * FROM users WHERE id = {}'.format(ctx.author.id))
-        row = cursor.fetchone()
+        row = users.find_one(id=ctx.author.id)
 
         if not row:
             await send_embed(ctx, 'Type !bal to set up your account!')
             return
         else:
-            new_bal = row[2] + int(amount)
-            cursor.execute('UPDATE users SET balance = {} WHERE id = {}'.format(new_bal, ctx.author.id))
-            db.commit()
-
+            new_bal = row['balance'] + int(amount)
+            users.update(dict(id=ctx.author.id, balance=new_bal), ['id'])
             await send_embed(ctx, '{}, I have added ${} to your account\nnew bal: ${}'
                              .format(ctx.author.name, amount, new_bal))
 
@@ -124,21 +118,20 @@ class Economy(commands.Cog):
                       help="Put your money on the line in hopes to double the amount you enter. "
                            "A roll over 50 (excluding) is a win")
     async def gamble(self, ctx, amount):
-        cursor.execute('SELECT * FROM users WHERE id = {}'.format(ctx.author.id))
-        row = cursor.fetchone()
+        row = users.find_one(id=ctx.author.id)
 
         if not row:
             await send_embed(ctx, 'Type !bal to set up your account!')
             return
-        elif int(amount) > row[2]:
-            await send_embed(ctx, 'You cannot gamble more than you have.\nBalance: ${}'.format(row[2]))
+        elif int(amount) > row['balance']:
+            await send_embed(ctx, 'You cannot gamble more than you have.\nBalance: ${}'.format(row['balance']))
             return
         elif int(amount) < 5:
             await send_embed(ctx, 'Minimum bet is $5')
             return
         else:
             num = random.randint(1, 100)
-            new_bal = row[2]
+            new_bal = row['balance']
             if num <= 50:
                 new_bal -= int(amount)
                 msg = 'Unfortunately you rolled {}. You lost ${}'.format(num, amount)
@@ -146,8 +139,7 @@ class Economy(commands.Cog):
                 new_bal += int(amount)
                 msg = 'Congrats! you rolled {}. You won ${}'.format(num, amount)
 
-            cursor.execute('UPDATE users SET balance = {} WHERE id = {}'.format(new_bal, ctx.author.id))
-            db.commit()
+            users.update(dict(id=ctx.author.id, balance=new_bal), ['id'])
             await send_embed(ctx, '{}\nBalance: ${}'.format(msg, new_bal))
 
 
@@ -161,65 +153,55 @@ class Games(commands.Cog):
                            "the game if needed.",
                       aliases=["tictactoe"])
     async def ttt(self, ctx):
-        cursor.execute('SELECT * FROM ttt_games WHERE noughts = {0} OR crosses = {0}'.format(ctx.author.id))
-        row = cursor.fetchone()
+        row = games.find_one((games.table.c.noughts == ctx.author.id) | (games.table.c.crosses == ctx.author.id))
 
         if not row:
-            cursor.execute('INSERT INTO ttt_games(crosses, guild_id) '
-                           'VALUES ({}, {})'.format(ctx.author.id, ctx.guild.id))
-            db.commit()
-
-            cursor.execute('SELECT game_id FROM ttt_games WHERE crosses = {}'.format(ctx.author.id))
-            game_id = cursor.fetchone()[0]
-            await send_embed(ctx, '{} has started a Tic Tac Toe game! Type !accept {} to join.'.format(ctx.author.name,
-                                                                                                       game_id))
+            games.insert(dict(crosses=ctx.author.id, guild_id=ctx.guild.id))
+            game_id = games.find_one(crosses=ctx.author.id)['game_id']
+            await send_embed(ctx, '{} has started a Tic Tac Toe game! Type !accept {} to join.'
+                             .format(ctx.author.name, game_id))
         else:
             await send_embed(ctx, 'You are already in a game!')
 
     @commands.command(hidden=True)
     async def accept(self, ctx, game_id):
-        cursor.execute('SELECT * FROM ttt_games WHERE game_id = {}'.format(game_id))
-        row = cursor.fetchone()
-
-        cursor.execute('SELECT * FROM ttt_games WHERE noughts = {0} OR crosses = {0}'.format(ctx.author.id))
-        in_game = cursor.fetchone()
+        row = games.find_one(game_id=game_id)
+        in_game = games.find_one((games.table.c.noughts == ctx.author.id) | (games.table.c.crosses == ctx.author.id))
 
         if not row:
             await send_embed(ctx, 'Invalid game id, or that game doesn\'t exist.')
             return
-        elif ctx.author.id == row[2]:
+        elif ctx.author.id == row['crosses']:
             await send_embed(ctx, 'You cannot join your own game!')
             return
         elif in_game:
             await send_embed(ctx, 'You are already in a game!')
             return
         else:
-            cursor.execute('UPDATE ttt_games SET noughts = {} WHERE game_id = {}'.format(ctx.author.id, game_id))
-            db.commit()
+            games.update(dict(game_id=game_id, noughts=ctx.author.id), ['game_id'])
             await send_embed(ctx, 'You have successfully joined the game! <@{}> type `!place <position>` to start!'
-                             .format(row[2]))
-            ttt_message = await send_embed(ctx, '```'
-                                                '1 | 2 | 3\t\t\t Type !place <position> to play your turn.\n'
-                                                '---------\t\t\t Get three in a row/diagonal to win.\n'
-                                                '4 | 5 | 6\t\t\t It is currently crosses\' turn.\n'
-                                                '---------\t\t\t Type !end to end the game.\n'
-                                                '7 | 8 | 9\n'
-                                                '```')
+                             .format(row['crosses']))
+            ttt_message: Message = await send_embed(ctx, '```'
+                                                         '1 | 2 | 3\t\t\t Type !place <position> to play your turn.\n'
+                                                         '---------\t\t\t Get three in a row/diagonal to win.\n'
+                                                         '4 | 5 | 6\t\t\t It is currently crosses\' turn.\n'
+                                                         '---------\t\t\t Type !end to end the game.\n'
+                                                         '7 | 8 | 9\n'
+                                                         '```')
 
-            cursor.execute('UPDATE ttt_games SET message = {} WHERE game_id = {}'.format(ttt_message.id, row[0]))
+            games.update(dict(game_id=game_id, message=ttt_message.id), row['game_id'])
 
     @commands.command(hidden=True)
     async def place(self, ctx, arg):
-        cursor.execute('SELECT * FROM ttt_games WHERE noughts = {0} OR crosses = {0}'.format(ctx.author.id))
-        game = cursor.fetchone()
+        row = games.find_one((games.table.c.noughts == ctx.author.id) | (games.table.c.crosses == ctx.author.id))
 
-        if not game:
+        if not row:
             await send_embed(ctx, 'You are not in a game! Type `!ttt` to start one.')
             return
-        elif not game[5]:
+        elif not row['message']:
             await send_embed(ctx, 'The game hasn\'t started yet!')
             return
-        elif game[game[3]] != ctx.author.id:
+        elif row[row['turn']] != ctx.author.id:
             await send_embed(ctx, 'It is not your turn.')
             return
         elif arg not in [str(i) for i in range(1, 10)]:
@@ -229,29 +211,25 @@ class Games(commands.Cog):
         cmd = await ctx.fetch_message(ctx.message.id)
         await cmd.delete()
 
-        symbol = "X" if game[3] == 2 else "O"
-        cursor.execute('SELECT positions FROM ttt_games WHERE game_id = {}'.format(game[0]))
-        r = cursor.fetchone()[0]
-        p = json.loads(r)
+        symbol = "X" if row['turn'] == 'crosses' else "O"
+        positions = games.find_one(game_id=row['game_id'])['positions']
+        p = json.loads(positions)
         if p[arg] == "X" or p[arg] == "O":
             await send_embed(ctx, 'That spot is already taken!')
             return
         else:
             p[arg] = symbol
         p = json.dumps(p)
-        cursor.execute("UPDATE ttt_games SET positions = '{}' WHERE game_id = {}".format(p, game[0]))
-        db.commit()
+        games.update(dict(game_id=row['game_id'], positions=p), ['game_id'])
 
-        cursor.execute('SELECT positions FROM ttt_games WHERE game_id = {}'.format(game[0]))
-        r = cursor.fetchone()[0]
-        p = json.loads(r)
+        positions = games.find_one(game_id=row['game_id'])['positions']
+        p = json.loads(positions)
 
         def end_game():
-            cursor.execute('DELETE FROM ttt_games WHERE game_id = {}'.format(game[0]))
-            db.commit()
+            games.delete(game_id=row['game_id'])
 
-        msg = await ctx.fetch_message(game[5])
-        await msg.edit(embed=discord.Embed(
+        msg = await ctx.fetch_message(row['message'])
+        await msg.edit(embed=Embed(
             description='```'
                         '{0} | {1} | {2}\t\t\t Type !place <position> to play your turn\n'
                         '---------\t\t\t Get three in a row/diagonal to win.\n'
@@ -259,18 +237,17 @@ class Games(commands.Cog):
                         '---------\n'
                         '{6} | {7} | {8}\n'
                         '```'.format(p["1"], p["2"], p["3"], p["4"], p["5"],
-                                     p["6"], p["7"], p["8"], p["9"],
-                                     ("crosses" if game[3] == 1 else "noughts"))))
+                                     p["6"], p["7"], p["8"], p["9"], row['turn'])))
 
         for h in [p["1"] + p["2"] + p["3"],
                   p["4"] + p["5"] + p["6"],
                   p["7"] + p["8"] + p["9"]]:
             if h == "XXX":
-                await send_embed(ctx, "<@{}> has won on Rows!".format(game[2]))
+                await send_embed(ctx, "<@{}> has won on Rows!".format(row['crosses']))
                 end_game()
                 return
             elif h == "OOO":
-                await send_embed(ctx, "<@{}> has won on Rows!".format(game[1]))
+                await send_embed(ctx, "<@{}> has won on Rows!".format(row['noughts']))
                 end_game()
                 return
 
@@ -278,22 +255,22 @@ class Games(commands.Cog):
                   p["2"] + p["5"] + p["8"],
                   p["3"] + p["6"] + p["9"]]:
             if v == "XXX":
-                await send_embed(ctx, "<@{}> has won on Columns!".format(game[2]))
+                await send_embed(ctx, "<@{}> has won on Columns!".format(row['crosses']))
                 end_game()
                 return
             elif v == "OOO":
-                await send_embed(ctx, "<@{}> has won on Columns!".format(game[1]))
+                await send_embed(ctx, "<@{}> has won on Columns!".format(row['noughts']))
                 end_game()
                 return
 
         for d in [p["1"] + p["5"] + p["9"],
                   p["3"] + p["5"] + p["7"]]:
             if d == "XXX":
-                await send_embed(ctx, "<@{}> has won on Diagonals!".format(game[2]))
+                await send_embed(ctx, "<@{}> has won on Diagonals!".format(row['crosses']))
                 end_game()
                 return
             elif d == "OOO":
-                await send_embed(ctx, "<@{}> has won on Diagonals!".format(game[1]))
+                await send_embed(ctx, "<@{}> has won on Diagonals!".format(row['noughts']))
                 end_game()
                 return
 
@@ -302,20 +279,17 @@ class Games(commands.Cog):
             end_game()
             return
 
-        next_turn = 1 if game[3] == 2 else 2
-        cursor.execute('UPDATE ttt_games SET turn = {} WHERE game_id = {}'.format(next_turn, game[0]))
-        db.commit()
+        next_turn = 'noughts' if row['turn'] == 'crosses' else 'crosses'
+        games.update(dict(game_id=row['game_id'], turn=next_turn), ['game_id'])
 
     @commands.command(hidden=True)
     async def end(self, ctx):
-        cursor.execute('SELECT * FROM ttt_games WHERE noughts = {0} OR crosses = {0}'.format(ctx.author.id))
-        game = cursor.fetchone()
+        row = games.find_one((games.table.c.noughts == ctx.author.id) | (games.table.c.crosses == ctx.author.id))
 
-        if not game:
+        if not row:
             await send_embed(ctx, 'You are not in a game! Type `!ttt` to start one.')
         else:
-            cursor.execute('DELETE FROM ttt_games WHERE game_id = {}'.format(game[0]))
-            db.commit()
+            games.delete(game_id=row['game_id'])
             await send_embed(ctx, '{} has ended the game.'.format(ctx.author.name))
 
 
